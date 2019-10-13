8db2f80810fa73f7ea50e83ebd6cdaa89d483a40
cross000 <39590588+cross000@users.noreply.github.com>
2019-10-13 19:05:56 +1000

    Layout Tabs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { Card, Button } from "react-bootstrap";
 import Arrow from "react-arrow";
 import ScrollUpButton from "react-scroll-up-button";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 function App() {
   return (
     <>
@@ -12,7 +14,6 @@ function App() {
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossorigin="anonymous"
       />
-      <link href="css/hover.css" rel="stylesheet" media="all"></link>
       <div className="App">
         <div className="sticky" style={{ paddingBottom: "7%" }}>
           <div className=" Title1 ">
@@ -26,23 +27,26 @@ function App() {
           className="sticky"
           style={{
             position: "sticky",
-            paddingTop: "33%",
+            paddingTop: "20%",
             color: "white"
           }}
         >
-          <h3>Computer Science student at QUT</h3>
+          <h3>
+            <mark>Computer Science student at QUT</mark>
+          </h3>
           <Arrow
+            className="zoom2"
             direction="down"
             shaftWidth={10}
             shaftLength={0}
             headWidth={50}
             headLength={40}
             fill="white"
-            stroke="#1F263"
-            strokeWidth={0.5}
+            stroke="black"
+            strokeWidth={1}
             onClick={() =>
               window.scrollTo({
-                top: 1000,
+                top: 745,
                 bottom: 0,
                 behavior: "smooth"
               })
@@ -50,146 +54,154 @@ function App() {
           />
         </div>
       </div>
-      <h1
-        style={{
-          textAlign: "center",
-          backgroundColor: "#1F2638",
-          color: "white"
-        }}
-      >
-        Recent Projects
-      </h1>
-      <div>
-        <ScrollUpButton />
-      </div>
-      <div
-        style={{
-          minHeight: "95vh",
-          paddingTop: "5%",
-          paddingLeft: "15%",
-          paddingRight: "15%",
+      <ScrollUpButton />
 
-          zIndex: "10",
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          backgroundColor: "#2B314B",
-          justifyContent: "center"
-        }}
-      >
-        <Card
-          bg="secondary"
-          className="zoom"
-          text="white"
-          style={{ width: "18rem", height: "35rem" }}
+      <Tabs>
+        <TabList
+          style={{
+            backgroundColor: "#2B314B",
+            color: "white",
+            textAlign: "center",
+            fontSize: "150%"
+          }}
         >
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card
-          bg="secondary"
-          className="zoom"
-          text="white"
-          style={{ width: "18rem", height: "35rem" }}
-        >
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card
-          bg="secondary"
-          className="zoom"
-          text="white"
-          style={{ width: "18rem", height: "35rem" }}
-        >
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card
-          bg="secondary"
-          className="zoom"
-          text="white"
-          style={{ width: "18rem", height: "35rem" }}
-        >
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card
-          bg="secondary"
-          className="zoom"
-          text="white"
-          style={{ width: "18rem", height: "35rem" }}
-        >
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card
-          bg="secondary"
-          className="zoom"
-          text="white"
-          style={{ width: "18rem", height: "35rem" }}
-        >
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card
-          bg="secondary"
-          className="zoom"
-          text="white"
-          style={{ width: "18rem", height: "35rem" }}
-        >
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </div>
+          <Tab>About Me</Tab>
+          <Tab>My Projects</Tab>
+        </TabList>
+
+        <TabPanel></TabPanel>
+        <TabPanel>
+          <div
+            style={{
+              minHeight: "50vh",
+              paddingTop: "5%",
+              paddingLeft: "15%",
+              paddingRight: "15%",
+
+              zIndex: "10",
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              backgroundColor: "#2B314B",
+              justifyContent: "center"
+            }}
+          >
+            <Card
+              bg="secondary"
+              className="zoom"
+              text="white"
+              style={{ width: "18rem", height: "35rem" }}
+            >
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card
+              bg="secondary"
+              className="zoom"
+              text="white"
+              style={{ width: "18rem", height: "35rem" }}
+            >
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card
+              bg="secondary"
+              className="zoom"
+              text="white"
+              style={{ width: "18rem", height: "35rem" }}
+            >
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card
+              bg="secondary"
+              className="zoom"
+              text="white"
+              style={{ width: "18rem", height: "35rem" }}
+            >
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card
+              bg="secondary"
+              className="zoom"
+              text="white"
+              style={{ width: "18rem", height: "35rem" }}
+            >
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card
+              bg="secondary"
+              className="zoom"
+              text="white"
+              style={{ width: "18rem", height: "35rem" }}
+            >
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card
+              bg="secondary"
+              className="zoom"
+              text="white"
+              style={{ width: "18rem", height: "35rem" }}
+            >
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>{" "}
+        </TabPanel>
+      </Tabs>
+      <div></div>
     </>
   );
 }
